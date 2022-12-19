013bad9aa33eef409453ec2922f7bb835cf2bfc7
tesla <lefthandmastery@gmail.com>
2022-12-19 21:59:23 +0800

    Updated formatter

--- proxy_finder/utils/formatter.py
+++ proxy_finder/utils/formatter.py
@@ -1,8 +1,13 @@
+from datetime import date
 
-def format_sitename(url: str):
+def format_sitename(url: str) -> str:
 
     if not url:
         return None
 
-    url = url.replace('http://', '').replace('https://', '').replace('www.','')
-    return url
+    return url.lower().replace('http://', '').replace('https://', '').replace('www.','').strip('/')
+
+
+def format_date(date: date):
+
+    return date.strftime('%Y_%m_%d')
